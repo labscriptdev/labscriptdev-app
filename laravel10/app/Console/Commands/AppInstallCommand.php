0d836e0c3bfdbf0b5987a0f4312fddc0a7f85b7c
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-06-16 02:30:52 -0300

    sync

--- laravel10/app/Console/Commands/AppInstallCommand.php
+++ laravel10/app/Console/Commands/AppInstallCommand.php
@@ -7,7 +7,7 @@ use App\Models\AppMailTemplate;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
  
-class AppInstall extends Command
+class AppInstallCommand extends Command
 {
     /**
      * The name and signature of the console command.
