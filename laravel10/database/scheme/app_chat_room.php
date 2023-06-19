bddedbd31732a5686d7fc1477eed0a73bd938bbf
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-06-18 22:11:02 -0300

    database scheme

--- laravel10/database/scheme/app_chat_room.php
+++ laravel10/database/scheme/app_chat_room.php
@@ -2,16 +2,13 @@
   'pk' => 'id',
   'fields' => [
     'id' => 'BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT',
-    'name' => 'VARCHAR(255) DEFAULT NULL',
-    'user_id' => 'BIGINT(20) UNSIGNED DEFAULT NULL',
-    'status' => 'ENUM("unread", "read") DEFAULT "unread"',
-    'url' => 'VARCHAR(255) DEFAULT NULL',
-    'text' => 'TEXT DEFAULT NULL',
+    'name' => 'VARCHAR(255) NULL DEFAULT NULL',
+    'owner_id' => 'BIGINT(20) UNSIGNED NULL DEFAULT NULL',
     'created_at' => 'TIMESTAMP NULL DEFAULT NULL',
     'updated_at' => 'TIMESTAMP NULL DEFAULT NULL',
   ],
   'fks' => [
-    'user_id' => [
+    'owner_id' => [
       'to_table' => 'app_user',
       'to_field' => 'id',
       'on_update' => 'NO ACTION',
--- laravel10/database/scheme/app_chat_room_message.php
+++ laravel10/database/scheme/app_chat_room_message.php
@@ -2,16 +2,15 @@
   'pk' => 'id',
   'fields' => [
     'id' => 'BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT',
-    'name' => 'VARCHAR(255) DEFAULT NULL',
-    'user_id' => 'BIGINT(20) UNSIGNED DEFAULT NULL',
+    'name' => 'VARCHAR(255) NULL DEFAULT NULL',
     'status' => 'ENUM("unread", "read") DEFAULT "unread"',
-    'url' => 'VARCHAR(255) DEFAULT NULL',
-    'text' => 'TEXT DEFAULT NULL',
+    'from_id' => 'BIGINT(20) UNSIGNED NULL DEFAULT NULL',
+    'to_id' => 'BIGINT(20) UNSIGNED NULL DEFAULT NULL',
     'created_at' => 'TIMESTAMP NULL DEFAULT NULL',
     'updated_at' => 'TIMESTAMP NULL DEFAULT NULL',
   ],
   'fks' => [
-    'user_id' => [
+    'owner_id' => [
       'to_table' => 'app_user',
       'to_field' => 'id',
       'on_update' => 'NO ACTION',
