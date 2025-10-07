dbcb954b6d81c48dad2bf10dd9c1b2fb78b80d2b
Jeferson Silva <jeferson.i.silva@gmail.com>
2025-10-07 11:53:24 -0300

    feat: keycloak verify auth

--- frontend/app/app/app.vue
+++ frontend/app/app/app.vue
@@ -1,5 +1,27 @@
 <template>
-  <nuxt-page />
+  <div>
+    <v-fade-transition>
+      <div
+        v-if="!app.ready"
+        class="bg-surface"
+        style="
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100vw;
+          height: 100vh;
+          z-index: 99;
+        "
+      >
+        Loading...
+      </div>
+    </v-fade-transition>
+
+    <nuxt-page v-if="app.ready" />
+  </div>
 </template>
 
 <script setup>
