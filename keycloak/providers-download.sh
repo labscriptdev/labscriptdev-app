6c466b8e6a11d8919fd80272ee07271890dc681f
Jeferson Silva <jeferson.i.silva@gmail.com>
2025-10-07 18:25:14 -0300

    feat: rabbitmq: config

--- keycloak/providers-download.sh
+++ keycloak/providers-download.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-curl -L -o ./providers/keycloak-webhook-provider-core.jar https://github.com/vymalo/keycloak-webhook/releases/download/v0.10.0-rc.1/keycloak-webhook-provider-core-0.10.0-rc.1-all.jar;
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+curl -L -o "$DIR/providers/keycloak-webhook-provider-core.jar" https://github.com/vymalo/keycloak-webhook/releases/download/v0.10.0-rc.1/keycloak-webhook-provider-core-0.10.0-rc.1-all.jar;
+curl -L -o "$DIR/providers/keycloak-webhook-provider-http.jar" https://github.com/vymalo/keycloak-webhook/releases/download/v0.10.0-rc.1/keycloak-webhook-provider-http-0.10.0-rc.1-all.jar;
+curl -L -o "$DIR/providers/keycloak-webhook-provider-amqp.jar" https://github.com/vymalo/keycloak-webhook/releases/download/v0.10.0-rc.1/keycloak-webhook-provider-amqp-0.10.0-rc.1-all.jar;
+# curl -L -o "$DIR/providers/keycloak-to-rabbit-3.0.5.jar" https://github.com/aznamier/keycloak-event-listener-rabbitmq/releases/download/3.0.5/keycloak-to-rabbit-3.0.5.jar;
