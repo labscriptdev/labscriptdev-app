d219b161c7e9e44c35a307f44867e34020bea1fe
Jeferson Silva <jeferson.i.silva@gmail.com>
2025-10-08 15:20:26 -0300

    feat: backend: config

--- backend/app/app/Console/Commands/AppTestCommand.php
+++ backend/app/app/Console/Commands/AppTestCommand.php
@@ -26,8 +26,16 @@ class AppTestCommand extends Command
      */
     public function handle()
     {
-        AppConfig::set('secret.root_user_id', null);
-        $id = AppConfig::get('secret.root_user_id');
-        dump($id);
+        // AppConfig::set('secret.root_user_id', null);
+        // $id = AppConfig::get('secret.root_user_id');
+        // dump($id);
+
+        // dump(AppConfig::get('secret'));
+        // dump(AppConfig::get('secret.root_user_id'));
+        // dump(AppConfig::set('secret', 123));
+        // dump(AppConfig::set('secret', ['root_user_id' => '68e5f51f03f7240a41019a04']));
+        // dump(AppConfig::set('secret.root_user_id', '68e5f51f03f7240a41019a04'));
     }
 }
+
+// 68e5f51f03f7240a41019a04
--- backend/app/app/Models/AppConfig.php
+++ backend/app/app/Models/AppConfig.php
@@ -12,23 +12,42 @@ class AppConfig extends Model
 
     static function get($path, $default = null)
     {
-        $paths = explode('.', $path);
-        $name = array_shift($paths);
-        $configPath = implode('.', $paths);
-        $appConfig = AppConfig::firstOrNew(['name' => $name], ['config' => []]);
-        $config = array_merge(config("app_config.{$name}", []), $appConfig->config);
-        return Arr::get($config, $configPath, $default);
+        $scope = self::pathInfo($path);
+        $default = config("app_config.{$scope->name}", []);
+        $default = Arr::get($default, $scope->path, $default);
+        $appConfig = AppConfig::firstOrNew(['name' => $scope->name], ['config' => []]);
+        $config = Arr::get($appConfig->config, $scope->path, $default);
+        if (is_array($default)) $config = array_merge($default, $config);
+        return $config;
     }
 
     static function set($path, $value)
     {
-        $paths = explode('.', $path);
-        $name = array_shift($paths);
-        $configPath = implode('.', $paths);
-        $appConfig = AppConfig::firstOrNew(['name' => $name], ['config' => []]);
-        $config = array_merge(config("app_config.{$name}", []), $appConfig->config);
-        $appConfig->config = Arr::set($config, $configPath, $value);
+        $scope = self::pathInfo($path);
+        $default = config("app_config.{$scope->name}", []);
+        $appConfig = AppConfig::firstOrNew(['name' => $scope->name], ['config' => []]);
+        $config = array_merge($default, $appConfig->config);
+
+        if ($scope->path) {
+            Arr::set($config, $scope->path, $value);
+        } else {
+            if (is_array($value)) {
+                $config = array_merge($config, $value);
+            }
+        }
+
+        $appConfig->config = $config;
         $appConfig->save();
-        return $appConfig->config;
+        return $config;
+    }
+
+    protected static function pathInfo($path)
+    {
+        $scope = (object) ['name' => null, 'path' => null];
+        $scope->path = explode('.', $path);
+        $scope->name = array_shift($scope->path);
+        $scope->path = join('.', $scope->path);
+        $scope->path = $scope->path ? $scope->path : null;
+        return $scope;
     }
 }
